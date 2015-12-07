991d20ead4c784f167b55a259bd14d7bdd63bdde
Mario Linz <Mario Linz>
2015-12-08 00:22:05 +0100

    Initial commit ...

---
+++ docs/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "jsCow.res.components.group",
+        "jsCow.res.controller.group",
+        "jsCow.res.model.group",
+        "jsCow.res.view.group"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
